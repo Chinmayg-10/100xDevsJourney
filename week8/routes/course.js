1ec0e79653796ec579dd5f97251f0224be5da74a
Chinmayg-10 <chinmaygupta032@gmail.com>
2025-02-25 12:53:56 +0530

    making more pretty

--- week8/routes/course.js
+++ week8/routes/course.js
@@ -1,41 +1,34 @@
-// function CreateCourseRoutes(app){
-//     app.post("/course/purchase",function(req,res){
-
-//     })
-//     app.get("/seeAllCourses",function(req,res){
-    
-//     })
-// }
-// module.exports={
-//     CreateCourseRoutes:CreateCourseRoutes
-// }
-
-const {Router}=require("express");
+const { Router } = require("express");
 const { purchaseModel, courseModel } = require("../Db");
-const {Usermiddleware}=require("../middleware/user")
-const CourseRouter=Router();
+const { Usermiddleware } = require("../middleware/user");
+const CourseRouter = Router();
 
-    CourseRouter.post("/purchase",Usermiddleware,async function(req,res){
-        const userId=req.userId;
-        const courseId=req.body.courseId;
-        //you should check wheather the user has paid the price
-        await purchaseModel.create({
-            userId,
-            courseId
-        });
-        res.json({
-            message:"course purchased"
-        })
-    });
-    CourseRouter.get("/preview",async function(req,res){
-        const courses=await courseModel.find({
+CourseRouter.post("/purchase", Usermiddleware, async function (req, res) {
+  const userId = req.userId;
+  const courseId = req.body.courseId;
 
-        })
-        res.json({
-            courses
-        })
-    })
+  console.log("Received userId:", userId);
+    console.log("Received courseId:", courseId);
 
-module.exports={
-    CourseRouter:CourseRouter
-}
+    if (!userId || !courseId) {
+        return res.status(400).json({ message: "Missing userId or courseId" });
+    }
+  //you should check wheather the user has paid the price
+  await purchaseModel.create({
+    userId,
+    courseId,
+  });
+  res.json({
+    message: "course purchased",
+  });
+});
+CourseRouter.get("/preview", async function (req, res) {
+  const courses = await courseModel.find({});
+  res.json({
+    courses,
+  });
+});
+
+module.exports = {
+  CourseRouter: CourseRouter,
+};
--- week8/routes/user.js
+++ week8/routes/user.js
@@ -1,86 +1,91 @@
-// function CreateUserRoutes(app){
-//     app.post("/signup",function(req,res){
-
-//     })
-//     app.post("login",function(req,res){
-    
-//     })
-//     app.get("/seeMyCourses",function(req,res){
-    
-//     })
-// }
-// module.exports={
-//     CreateUserRoutes:CreateUserRoutes
-// }
-const bcrypt=require("bcrypt");
-const {z}=require("zod");
-const {Router}=require("express");
-const userRouter=Router();
-const jwt=require("jsonwebtoken");
-const JWT_USER_PASSWORD="bhcbbsjdnu"
-const {userModel,purchaseModel}=require("../Db.js")
-const {Usermiddleware}=require("../middleware/user.js")
-    userRouter.post("/signup",async function(req,res){
-        const requiredBody=z.object({
-            email:z.string().min(10).max(50).email(),
-            password:z.string().min(7).max(15),
-            firstName:z.string().min(3).max(15),
-            lastName:z.string().min(3).max(15)
-        })
-        const passDatawithSuccess=requiredBody.safeParse(req.body);
-        if(!passDatawithSuccess.success){
-            return res.status(400).json({ message: "Format Error", errors: passDatawithSuccess.error.errors });
-        }
-        const {email,password,firstName,lastName}=req.body;
-        const hashedPwd=await bcrypt.hash(password,5);
-        await userModel.create({
-            email:email,
-            password:hashedPwd,
-            firstName:firstName,
-            lastName:lastName
-        })
-        res.json({
-            message:"User signup done!"
-        })
-
-    })
-    userRouter.post("/login",async function(req,res){
-        const {email,password}=req.body;
-        const user=await userModel.findOne({
-            email:email
-        })
-        if(!user){
-            res.status(403).json({
-                message:"User is not found in the Database"
-            })
-        }
-        const passwordMatch=bcrypt.compare(password,user.password);
-        if(passwordMatch){
-            const token=jwt.sign({
-                id:user._id.toString()
-            },JWT_USER_PASSWORD);
-            res.json({
-                token:token
-            })
-        }
-        else{
-            res.json({
-                message:"Incorrect Data Inserted!" 
-            })
-        }
-    
+const bcrypt = require("bcrypt");
+const { z } = require("zod");
+const { Router } = require("express");
+const userRouter = Router();
+const jwt = require("jsonwebtoken");
+const JWT_USER_PASSWORD = "bhcbbsjdnu";
+const { userModel, purchaseModel,courseModel } = require("../Db.js");
+const { Usermiddleware } = require("../middleware/user.js");
+userRouter.post("/signup", async function (req, res) {
+  const requiredBody = z.object({
+    email: z.string().min(10).max(50).email(),
+    password: z.string().min(7).max(15),
+    firstName: z.string().min(3).max(15),
+    lastName: z.string().min(3).max(15),
+  });
+  const passDatawithSuccess = requiredBody.safeParse(req.body);
+  if (!passDatawithSuccess.success) {
+    return res.status(400).json({
+      message: "Format Error",
+      errors: passDatawithSuccess.error.errors,
     });
-    userRouter.get("/seeMyCourses",Usermiddleware,async function(req,res){
-        const userId=req.userId;
-        const purchased=await purchaseModel.find({
-            userId
-        });
-        res.json({
-            message:"All courses purchased are :",
-            purchased
-        })
-    })
+  }
+  const { email, password, firstName, lastName } = req.body;
+  const hashedPwd = await bcrypt.hash(password, 5);
+  await userModel.create({
+    email: email,
+    password: hashedPwd,
+    firstName: firstName,
+    lastName: lastName,
+  });
+  res.json({
+    message: "User signup done!",
+  });
+});
+userRouter.post("/login", async function (req, res) {
+  const { email, password } = req.body;
+  const user = await userModel.findOne({
+    email: email,
+  });
+  if (!user) {
+    res.status(403).json({
+      message: "User is not found in the Database",
+    });
+  }
+  const passwordMatch =bcrypt.compare(password, user.password);
+  if (passwordMatch) {
+    const token = jwt.sign(
+      {
+        id: user._id.toString(),
+      },
+      JWT_USER_PASSWORD
+    );
+    res.json({
+      token: token,
+    });
+  } else {
+    res.json({
+      message: "Incorrect Data Inserted!",
+    });
+  }
+});
 
-module.exports={
-    userRouter:userRouter
+///not working properly
+userRouter.get("/seeMyCourses", Usermiddleware, async function (req, res) {
+  const userId = req.userId;
+  const purchased = await purchaseModel.find({
+    userId:userId,
+  });
+  if (!purchased) {
+    return res.status(404).json({
+        message: "No purchases found", // Error message for no purchases found
+    });
 }
+// If purchases are found, extract the courseIds from the found purchases
+const purchasesCourseIds = purchased.map((purchase) => purchase.course_id);
+
+// Find all course details associated with the courseIds
+const coursesData = await courseModel.find({
+    _id: { $in: purchasesCourseIds }, // Querying courses using the extracted course IDs
+});
+
+  res.json({
+    message: "All courses purchased are :",
+    purchased, // Include purchase data in the response
+    coursesData // Include course details in the response
+  });
+});
+
+module.exports = {
+  userRouter: userRouter,
+};
