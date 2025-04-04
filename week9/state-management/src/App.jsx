94f1392c71ea3b34bac8be3bc35758a948f05764
Chinmayg-10 <chinmaygupta032@gmail.com>
2025-04-04 16:35:29 +0530

    context API

--- week9/state-management/src/App.jsx
+++ week9/state-management/src/App.jsx
@@ -1,28 +1,105 @@
-import { useState } from 'react'
+import { createContext, useContext, useState } from 'react'
 
+// function App() {
+//   return <div>
+//     <LightBulb />
+//   </div>
+// }
+
+// function LightBulb() {
+//   const [bulbOn, setBulbOn] = useState(true)
+
+//   return <div>
+//     <BulbState bulbOn={bulbOn} />
+//     <ToggleBulbState setBulbOn={setBulbOn} />
+//   </div>
+// }
+
+// //bulbOn is a prop to BulbState component
+// function BulbState({bulbOn}) {
+//   return <div>
+//     {bulbOn ? "Bulb on" : "Bulb off"}
+//   </div>
+// }
+
+// function ToggleBulbState({setBulbOn}) {
+
+//   function toggle() {
+//     setBulbOn(c=>!c)
+//   }
+
+//   return <div>
+//     <button onClick={toggle}>Toggle the bulb</button>
+//   </div>
+// }
+
+//prop drilling
+// function App() {
+//   const [bulbOn, setBulbOn] = useState(true)
+//   return <div>
+//     <LightBulb bulbOn={bulbOn} setBulbOn={setBulbOn} />
+//   </div>
+// }
+
+// function LightBulb({bulbOn,setBulbOn}) {
+//   return <div>
+//     <BulbState bulbOn={bulbOn} />
+//     <ToggleBulbState setBulbOn={setBulbOn} />
+//   </div>
+// }
+
+// //bulbOn is a prop to BulbState component
+// function BulbState({bulbOn}) {
+//   return <div>
+//     {bulbOn ? "Bulb on" : "Bulb off"}
+//   </div>
+// }
+
+// function ToggleBulbState({setBulbOn}) {
+
+//   function toggle() {
+//     setBulbOn(c=>!c)
+//   }
+
+//   return <div>
+//     <button onClick={toggle}>Toggle the bulb</button>
+//   </div>
+// }
+
+//CONTEXT API
+const BulbContext=createContext();
+
+function BulbProvider({children}){
+  const [bulbOn, setBulbOn] = useState(true)
+  return <BulbContext.Provider value={{bulbOn:bulbOn, setBulbOn:setBulbOn}}>
+    {children}
+  </BulbContext.Provider>
+}
 function App() {
   return <div>
-    <LightBulb />
+    <BulbProvider>
+    <LightBulb/>
+    </BulbProvider>
   </div>
 }
 
 function LightBulb() {
-  const [bulbOn, setBulbOn] = useState(true)
-
   return <div>
-    <BulbState bulbOn={bulbOn} />
-    <ToggleBulbState setBulbOn={setBulbOn} />
+    <BulbState />
+    <ToggleBulbState/>
   </div>
 }
 
-function BulbState({bulbOn}) {
+//bulbOn is a prop to BulbState component
+function BulbState() {
+  const {bulbOn}=useContext(BulbContext);
   return <div>
     {bulbOn ? "Bulb on" : "Bulb off"}
   </div>
 }
 
-function ToggleBulbState({setBulbOn}) {
-
+function ToggleBulbState() {
+  const {setBulbOn}=useContext(BulbContext);
   function toggle() {
     setBulbOn(c=>!c)
   }
@@ -31,5 +108,4 @@ function ToggleBulbState({setBulbOn}) {
     <button onClick={toggle}>Toggle the bulb</button>
   </div>
 }
-
 export default App
